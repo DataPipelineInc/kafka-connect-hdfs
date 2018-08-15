c2c2da8624a358b8692a8fa3eb3a4affc7938e47
lvpeng <lvpeng@datapipeline.com>
2018-08-15 15:59:23 +0800

    add csv format

--- src/main/java/io/confluent/connect/hdfs/csv/CsvHiveFactory.java
+++ src/main/java/io/confluent/connect/hdfs/csv/CsvHiveFactory.java
@@ -1,5 +1,7 @@
 package io.confluent.connect.hdfs.csv;
 
+import io.confluent.connect.hdfs.HdfsSinkConnectorConfig;
+import io.confluent.connect.hdfs.hive.HiveMetaStore;
 import io.confluent.connect.storage.hive.HiveFactory;
 import io.confluent.connect.storage.hive.HiveUtil;
 import org.apache.kafka.common.config.AbstractConfig;
@@ -9,6 +11,10 @@ public class CsvHiveFactory implements HiveFactory {
   @Override
   public HiveUtil createHiveUtil(
       AbstractConfig config, io.confluent.connect.storage.hive.HiveMetaStore hiveMetaStore) {
-    return createHiveUtil(config, hiveMetaStore);
+    return createHiveUtil((HdfsSinkConnectorConfig) config, (HiveMetaStore) hiveMetaStore);
+  }
+
+  public HiveUtil createHiveUtil(HdfsSinkConnectorConfig config, HiveMetaStore hiveMetaStore) {
+    return new CsvHiveUtil(config, hiveMetaStore);
   }
 }
